30ad63331e07dce817b9019124e09e60976d8e5b
Prit Gondaliya <96789149+GondaliyaPrit@users.noreply.github.com>
2024-05-04 14:54:11 +0530

    change start line cordinatoe

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ class Game {
         if (this.currentLinePoints.length < 2) return;
     
         const newLine = {
-            start: this.currentLinePoints[this.currentLinePoints.length ], 
+            start: this.currentLinePoints[this.currentLinePoints.length-10  ], 
             end: this.currentLinePoints[this.currentLinePoints.length - 1]
         };
 
